0d9a54ec8c0362d6255c1ee4607dd6b2306bb294
stevecyj <ycsteven.tsao@gmail.com>
2024-04-01 08:20:27 +0800

    WIP: button component

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { ButtonProps } from './types'
+defineProps<ButtonProps>()
+</script>
+
+<template>
+  <button
+    class="s-button"
+    :class="{
+      [`s-button--${type}`]: type,
+      [`s-button--${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled
+    }"
+    :disabled="disabled"
+  >
+    <span>
+      <slot />
+    </span>
+  </button>
+</template>
+
+<style lang="scss" scoped></style>
