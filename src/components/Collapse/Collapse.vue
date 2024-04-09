b1c794cc0e2f7d9af29a38b7508e36c469d3a3de
steve.tsao <steve.tsao@hlwtech.com.tw>
2024-04-09 17:44:03 +0800

    WIP: component Collapse

---
+++ src/components/Collapse/Collapse.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'SCollapse'
+})
+</script>
+
+<template>
+  <div class="s-collapse">
+    <slot />
+  </div>
+</template>
---
+++ src/components/Collapse/CollapseItem.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import type { CollapseItemProps } from './types'
+defineOptions({
+  name: 'SCollapseItem'
+})
+defineProps<CollapseItemProps>()
+</script>
+
+<template>
+  <div class="s-collapse-item"></div>
+</template>
+
+<style scoped></style>
