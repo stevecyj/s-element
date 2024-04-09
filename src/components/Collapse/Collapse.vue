cd2b1135e97a3a22777d40c4fb60cfdb3a92875b
stevecyj <ycsteven.tsao@gmail.com>
2024-04-10 07:55:54 +0800

    feat: refactor Collapse component and related files
    - Add type imports for `NameType` and `collapseContextKey` in `Collapse.vue`
    - Define `activeNames` ref and `handleItemClick` function in `Collapse.vue`
    - Provide context values in `Collapse.vue`
    - Add imports for `collapseContextKey` in `CollapseItem.vue`
    - Define props and computed properties in `CollapseItem.vue`
    - Add `handleClick` function in `CollapseItem.vue`
    - Update the template structure in `CollapseItem.vue`
    - Add styles for `.s-collapse-item__header` in `CollapseItem.vue`
    - Update the `name` prop type to `NameType` in `types.ts`
    - Define `CollapseContext` interface in `types.ts`
    - Create `collapseContextKey` injection key in `types.ts`
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- src/components/Collapse/Collapse.vue
+++ src/components/Collapse/Collapse.vue
@@ -1,6 +1,25 @@
 <script setup lang="ts">
+import type { NameType } from './types'
+import { collapseContextKey } from './types'
+
 defineOptions({
   name: 'SCollapse'
+})
+
+const activeNames = ref<NameType[]>([])
+
+const handleItemClick = (name: NameType) => {
+  const index = activeNames.value.indexOf(name)
+  if (index === -1) {
+    activeNames.value.push(name)
+  } else {
+    activeNames.value.splice(index, 1)
+  }
+}
+
+provide(collapseContextKey, {
+  activeNames,
+  handleItemClick
 })
 </script>
 
--- src/components/Collapse/CollapseItem.vue
+++ src/components/Collapse/CollapseItem.vue
@@ -1,20 +1,46 @@
 <script setup lang="ts">
 import type { CollapseItemProps } from './types'
+import { collapseContextKey } from './types'
+
 defineOptions({
   name: 'SCollapseItem'
 })
-defineProps<CollapseItemProps>()
+
+const props = defineProps<CollapseItemProps>()
+
+const collapseContext = inject(collapseContextKey)
+
+const isActive = computed(() =>
+  collapseContext?.activeNames.value.includes(props.name)
+)
+
+const handleClick = () => {
+  if (props.disabled) return
+  collapseContext?.handleItemClick(props.name)
+}
 </script>
 
 <template>
   <div class="s-collapse-item" :class="{ 'is-disabled': disabled }">
-    <div class="s-collapse-item__header" :id="`item-header-${name}`">
+    <div
+      class="s-collapse-item__header"
+      :id="`item-header-${name}`"
+      @click="handleClick"
+    >
       <slot name="title">{{ title }}</slot>
     </div>
-    <div class="s-collapse-item__content" :id="`item-content-${name}`">
+    <div
+      class="s-collapse-item__content"
+      :id="`item-content-${name}`"
+      v-show="isActive"
+    >
       <slot />
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.s-collapse-item__header {
+  font-size: 30px;
+}
+</style>
