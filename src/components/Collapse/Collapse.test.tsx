bb354298f43c34c30c8ff651ef5e13dd99231117
stevecyj <ycsteven.tsao@gmail.com>
2024-04-19 21:02:28 +0800

    feat: refactor test suite for improved readability and maintainability
    - Add type declarations for VueWrapper and DOMWrapper
    - Define variables for headers, contents, and specific elements
    - Move initialization of variables to a beforeAll hook
    - Update tests to use the defined variables instead of finding elements again
    - Add comments for better understanding of the code logic
    - Update test expectations and assertions for better coverage
    - Remove console log statements
    - Add new test for emitting correct events
    - Add new test for no response when disabled
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- src/components/Collapse/Collapse.test.tsx
+++ src/components/Collapse/Collapse.test.tsx
@@ -1,12 +1,23 @@
 import { it, expect, describe } from 'vitest'
 import { mount } from '@vue/test-utils'
+import type { VueWrapper, DOMWrapper } from '@vue/test-utils'
 import Collapse from './Collapse.vue'
 import CollapseItem from './CollapseItem.vue'
 
+const onChange = vi.fn()
+
+let wrapper: VueWrapper
+let headers: DOMWrapper<Element>[], contents: DOMWrapper<Element>[]
+let firstHeader: DOMWrapper<Element>,
+  secondHeader: DOMWrapper<Element>,
+  disabledHeader: DOMWrapper<Element>
+let firstContent: DOMWrapper<Element>,
+  secondContent: DOMWrapper<Element>,
+  disabledContent: DOMWrapper<Element>
+
 describe('Collapse.vue', () => {
-  it('basic collapse', async () => {
-    const onChange = vi.fn()
-    const wrapper = mount(
+  beforeAll(() => {
+    wrapper = mount(
       () => (
         <Collapse modelValue={['a']} onChange={onChange}>
           <CollapseItem title="title a" name="a">
@@ -28,37 +39,54 @@ describe('Collapse.vue', () => {
       }
     )
     // console.log('collapse: ', wrapper.html())
-    const headers = wrapper.findAll('.s-collapse-item__header')
-    const contents = wrapper.findAll('.s-collapse-item__wrapper')
+    headers = wrapper.findAll('.s-collapse-item__header')
+    contents = wrapper.findAll('.s-collapse-item__wrapper')
 
+    // 內容
+    firstHeader = headers[0]
+    secondHeader = headers[1]
+    disabledHeader = headers[2]
+
+    firstContent = contents[0]
+    secondContent = contents[1]
+    disabledContent = contents[2]
+  })
+
+  it('basic structure, corresponding text', () => {
+    // length
     expect(headers.length).toBe(3)
     expect(contents.length).toBe(3)
 
-    const firstHeader = headers[0]
-    const secondHeader = headers[1]
+    // context
     expect(firstHeader.text()).toBe('title a')
-
-    // 內容
-    const firstContent = contents[0]
-    const secondContent = contents[1]
-    const disabledContent = contents[2]
     expect(firstContent.isVisible()).toBeTruthy()
+
     expect(secondContent.isVisible()).toBeFalsy()
     expect(firstContent.text()).toBe('content a')
+  })
 
+  it('click header to open/close content', async () => {
     // 點擊行為
     await firstHeader.trigger('click')
-    console.log(firstContent.html())
+    // console.log(firstContent.html())
     expect(firstContent.isVisible()).toBeFalsy()
-    expect(onChange).toHaveBeenCalledWith([])
     await secondHeader.trigger('click')
     expect(secondHeader.isVisible()).toBeTruthy()
-    expect(onChange).toHaveBeenLastCalledWith(['b'])
+  })
 
-    // disabled
-    const disabledHeader = headers[2]
+  it('emit correct event', () => {
+    expect(onChange).toHaveBeenCalledTimes(2)
+    expect(onChange).toHaveBeenCalledWith([])
+    expect(onChange).toHaveBeenLastCalledWith(['b'])
+  })
+
+  it('should no response for disabled', async () => {
+    onChange.mockClear()
+
     expect(disabledHeader.classes()).toContain('is-disabled')
     await disabledHeader.trigger('click')
     expect(disabledContent.isVisible()).toBeFalsy()
+
+    expect(onChange).not.toHaveBeenCalled()
   })
 })
