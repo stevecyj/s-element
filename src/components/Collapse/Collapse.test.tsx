881875d1f563692845c80bfc2d42fafdfec9b992
steve.tsao <steve.tsao@hlwtech.com.tw>
2024-04-19 15:39:00 +0800

    feat: refactor Vue test files for Collapse component
    - Delete the old Vue 2 test file
    - Add a new Vue 3 test file for Collapse component
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

---
+++ src/components/Collapse/Collapse.test.tsx
@@ -0,0 +1,61 @@
+import { it, expect, describe } from 'vitest'
+import { mount } from '@vue/test-utils'
+import Collapse from './Collapse.vue'
+import CollapseItem from './CollapseItem.vue'
+
+describe('Collapse.vue', () => {
+  it('basic collapse', async () => {
+    const wrapper = mount(
+      () => (
+        <Collapse modelValue={['a']}>
+          <CollapseItem title="title a" name="a">
+            content a
+          </CollapseItem>
+          <CollapseItem title="title b" name="b">
+            content b
+          </CollapseItem>
+          <CollapseItem title="title c" name="c" disabled>
+            content c
+          </CollapseItem>
+        </Collapse>
+      ),
+      {
+        global: {
+          stubs: ['Icon']
+        },
+        attachTo: document.body
+      }
+    )
+    // console.log('collapse: ', wrapper.html())
+    const headers = wrapper.findAll('.s-collapse-item__header')
+    const contents = wrapper.findAll('.s-collapse-item__wrapper')
+
+    expect(headers.length).toBe(3)
+    expect(contents.length).toBe(3)
+
+    const firstHeader = headers[0]
+    const secondHeader = headers[1]
+    expect(firstHeader.text()).toBe('title a')
+
+    // 內容
+    const firstContent = contents[0]
+    const secondContent = contents[1]
+    const disabledContent = contents[2]
+    expect(firstContent.isVisible()).toBeTruthy()
+    expect(secondContent.isVisible()).toBeFalsy()
+    expect(firstContent.text()).toBe('content a')
+
+    // 點擊行為
+    await firstHeader.trigger('click')
+    console.log(firstContent.html())
+    expect(firstContent.isVisible()).toBeFalsy()
+    await secondHeader.trigger('click')
+    expect(secondHeader.isVisible()).toBeTruthy()
+
+    // disabled
+    const disabledHeader = headers[2]
+    expect(disabledHeader.classes()).toContain('is-disabled')
+    await disabledHeader.trigger('click')
+    expect(disabledContent.isVisible()).toBeFalsy()
+  })
+})
